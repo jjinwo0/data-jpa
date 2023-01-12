b62660c62d442ae28127e1c9d45a8bef8133dd5b
Jinwoo Park <zvzv9808@gmail.com>
2023-01-12 17:43:28 +0900

    Domain Test: Member, Team

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,45 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+//jpa entity에서는 기본 생성자가 필요함
+//access level은 protected까지 열어두어야함
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@ToString(): 객체를 만들어낼 때 출력
+//연관관계 필드는 넣지 않는 것이 좋음
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    //jpa entity에서는 기본 생성자가 필요함
-    //access level은 protected까지 열어두어야함
-    protected Member(){
-    }
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null){
+            changeTeam(team);
+        }
+    }
+
+    //연관관계 메서드
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);//상속받은 JpaRepository 인터페이스에서 제공
+
+        //get(): Optional<>을 따와서 사용할 수 있음. (Null 가능성 내포)
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
