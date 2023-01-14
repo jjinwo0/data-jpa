0c433fdff8ae3f9c42f4c6ca5cc497428869d069
Jinwoo Park <zvzv9808@gmail.com>
2023-01-14 22:22:35 +0900

    common interface Utils

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -9,6 +9,7 @@ import study.datajpa.entity.Member;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -26,8 +27,8 @@ class MemberJpaRepositoryTest {
 
         Member findMember = memberJpaRepository.find(savedMember.getId());
 
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
     }
 
     @Test
@@ -38,14 +39,25 @@ class MemberJpaRepositoryTest {
         memberJpaRepository.save(member1);
         memberJpaRepository.save(member2);
 
+        //단건 조회 검증
         Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
-        Member findMember2 = memberJpaRepository.findById(member1.getId()).get();
-        Assertions.assertThat(findMember1).isEqualTo(member1);
-        Assertions.assertThat(findMember1).isEqualTo(member2);
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
 
         //리스트 조회 검증
         List<Member> all = memberJpaRepository.findAll();
-        Assertions.assertThat(all.size()).isEqualTo(2);
+        assertThat(all.size()).isEqualTo(2);
 
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
