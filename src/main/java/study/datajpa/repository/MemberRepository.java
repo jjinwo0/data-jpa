e6ce2b861398eff5cfa9da0d071023b1954e258e
Jinwoo Park <zvzv9808@gmail.com>
2023-01-21 17:03:18 +0900

    EntityGraph Annotation

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -45,4 +46,23 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     //clearAutomatically = true: em.clear() 동작을 알아서 수행함.
     @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    //fetch: member를 조회할 때 team도 한방에 끌어올 수 있음.
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = ("team")) //findAll동작 시 member 뿐만 아니라 team도 함께 조회하기 위해 추가
+    List<Member> findAll();
+
+    //JPQL 사용 시에도 @EntityGraph를 사용하여 fetch join이 가능함
+    @EntityGraph(attributePaths = ("team"))
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    @EntityGraph(attributePaths = ("team"))
+//    @EntityGraph("Member.all") //Member Entity에 선언한 NamedEntityGraph 실행
+    List<Member> findEntityGraphByUsername(@Param("username")String username);
+
+
 }
