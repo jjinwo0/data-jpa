3dd6866c86213e152bdf509083533d850cfdd84e
Jinwoo Park <zvzv9808@gmail.com>
2023-01-22 21:29:24 +0900

    JPA: Auditing

---
+++ src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass //진짜 상속 관계는 아니며, 클래스 내부 속성을 공유하기 위한 어노테이션 (쿼리 문에 속성으로 추가됨)
+public class JpaBaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    public void prePersist(){
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        updatedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedDate = LocalDateTime.now();
+    }
+}
